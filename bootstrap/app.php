fafbe069499c0184b7ad321be63738e65c4dcc3f
Nvlqmn <puremachine99@gmail.com>
2025-08-05 20:59:56 +0700

    [PORTL-43] Upload gambar on livewire Unauthorized fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '*'); // <- TRUST ALL PROXIES
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
