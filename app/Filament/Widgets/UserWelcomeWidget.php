a173b0f6f0ccde1dd3f88ae31f71960302771a48
Nvlqmn <puremachine99@gmail.com>
2025-08-05 20:37:01 +0700

    [PORTL-33] Custom widget welcome

--- app/Filament/Widgets/UserWelcomeWidget.php
+++ app/Filament/Widgets/UserWelcomeWidget.php
@@ -8,7 +8,7 @@ class UserWelcomeWidget extends Widget
 {
     protected static string $view = 'filament.widgets.user-welcome-widget';
 
-    protected static ?int $sort = -1; // biar tampil paling atas
+    protected static ?int $sort = -3; // biar tampil paling atas
 
     protected int|string|array $columnSpan = 'full';
 
