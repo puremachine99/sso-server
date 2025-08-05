18beb8a18ca83e8995dd2b04024240aa19652b20
Nvlqmn <puremachine99@gmail.com>
2025-08-05 20:30:35 +0700

    [PORTL-34] Email contact

--- resources/views/filament/widgets/credit.blade.php
+++ resources/views/filament/widgets/credit.blade.php
@@ -3,8 +3,8 @@
         <div class="flex items-center justify-between">
             <div>
                 <p class="text-sm text-gray-500">AIT • SMART ID © {{ date('Y') }} SmartID Portal</p>
-                <a href="mailto:support@yukikoi.id" class="text-sm text-primary-600 hover:underline">
-                    support@smartid.com
+                <a href="mailto:contact@smartid.co.id" class="text-sm text-primary-600 hover:underline">
+                    contact@smartid.co.id
                 </a>
             </div>
 
