40a818946b2bd59ce0fc47a9f5c68f21a521eb2d
Nvlqmn <puremachine99@gmail.com>
2025-08-06 11:19:57 +0700

    [PORTL-46] Bikin Display untuk user-profile

--- resources/views/livewire/my-custom-profile-component.blade.php
+++ resources/views/livewire/my-custom-profile-component.blade.php
@@ -1,26 +1,42 @@
 <x-filament::section>
     <x-slot name="header">
-        <h2 class="text-xl font-bold tracking-tight">
+        <h3 class="fi-section-header-heading text-base font-semibold leading-6 text-gray-950 dark:text-white">
             Profile Information
-        </h2>
-
-        <p class="text-sm text-gray-500 dark:text-gray-400">
-            This section displays your account name and email.
+        </h3>
+        <p class="fi-section-header-description text-sm text-gray-500 dark:text-gray-400">
+            View your account profile name and email address.
         </p>
     </x-slot>
 
-    <x-filament::grid>
-        <x-filament::card>
-            <dl class="text-sm text-gray-200">
-                <div class="mb-2">
-                    <dt class="font-medium text-white">Name</dt>
-                    <dd>{{ auth()->user()->name }}</dd>
+    <div class="fi-section-content-ctn rounded-xl bg-white shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10 md:col-span-2">
+        <div class="fi-section-content p-6">
+            <div class="grid grid-cols-1 gap-6">
+                <div class="fi-fo-field-wrp">
+                    <div class="grid gap-y-2">
+                        <div class="flex items-center gap-x-3 justify-between">
+                            <label class="fi-fo-field-wrp-label inline-flex items-center gap-x-3">
+                                <span class="text-sm font-medium leading-6 text-gray-950 dark:text-white">Name</span>
+                            </label>
+                        </div>
+                        <div class="fi-input-wrp flex rounded-lg shadow-sm ring-1 bg-white dark:bg-white/5 ring-gray-950/10 dark:ring-white/20 px-3 py-1.5">
+                            <span class="text-base text-gray-950 dark:text-white">{{ auth()->user()->name }}</span>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <dt class="font-medium text-white">Email</dt>
-                    <dd>{{ auth()->user()->email }}</dd>
+
+                <div class="fi-fo-field-wrp">
+                    <div class="grid gap-y-2">
+                        <div class="flex items-center gap-x-3 justify-between">
+                            <label class="fi-fo-field-wrp-label inline-flex items-center gap-x-3">
+                                <span class="text-sm font-medium leading-6 text-gray-950 dark:text-white">Email</span>
+                            </label>
+                        </div>
+                        <div class="fi-input-wrp flex rounded-lg shadow-sm ring-1 bg-white dark:bg-white/5 ring-gray-950/10 dark:ring-white/20 px-3 py-1.5">
+                            <span class="text-base text-gray-950 dark:text-white">{{ auth()->user()->email }}</span>
+                        </div>
+                    </div>
                 </div>
-            </dl>
-        </x-filament::card>
-    </x-filament::grid>
+            </div>
+        </div>
+    </div>
 </x-filament::section>
