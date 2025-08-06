4f3efedab36766158c2eff2aef7eb2572017092a
Nvlqmn <puremachine99@gmail.com>
2025-08-06 11:12:34 +0700

    [PORTL-46] Bikin Display untuk user-profile

--- app/Filament/Pages/MyCustomProfileComponent.php
+++ app/Filament/Pages/MyCustomProfileComponent.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
@@ -8,9 +9,13 @@ class MyCustomProfileComponent extends Page
     protected static ?string $navigationIcon = null;
     protected static string $view = 'filament.pages.my-custom-profile-component';
 
-    // Optional: supaya tidak muncul di sidebar, cukup dari plugin saja
     public static function shouldRegisterNavigation(): bool
     {
         return false;
+    }
+
+    public static function getSort(): ?int
+    {
+        return null; // Prevent error from Filament expecting this
     }
 }
