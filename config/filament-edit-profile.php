d2ccd3487d625f29bddffda345e6fca2ab4f8230
Nvlqmn <puremachine99@gmail.com>
2025-08-06 13:41:01 +0700

    [PORTL-40] buat ketentuan password

--- config/filament-edit-profile.php
+++ config/filament-edit-profile.php
@@ -4,6 +4,8 @@ return [
     'avatar_column' => 'avatar_url',
     'disk' => env('FILESYSTEM_DISK', 'public'),
     'visibility' => 'public',
+    'password_rules' => ['required', 'string', 'min:8', 'regex:/[A-Z]/', 'regex:/[0-9]/'],
+
     'show_custom_fields' => false,
     'custom_fields' => [
         'custom_field_1' => [
