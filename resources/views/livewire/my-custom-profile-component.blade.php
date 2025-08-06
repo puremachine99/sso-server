42da4524bdaae91b9d0711b74e7d48d118b39e61
Nvlqmn <puremachine99@gmail.com>
2025-08-06 11:17:31 +0700

    [PORTL-46] Bikin Display untuk user-profile

--- resources/views/livewire/my-custom-profile-component.blade.php
+++ resources/views/livewire/my-custom-profile-component.blade.php
@@ -1,10 +1,26 @@
 <x-filament::section>
     <x-slot name="header">
-        <h2 class="text-lg font-bold">Profil Saya</h2>
+        <h2 class="text-xl font-bold tracking-tight">
+            Profile Information
+        </h2>
+
+        <p class="text-sm text-gray-500 dark:text-gray-400">
+            This section displays your account name and email.
+        </p>
     </x-slot>
 
-    <x-filament::card>
-        <p><strong>Nama:</strong> {{ auth()->user()->name }}</p>
-        <p><strong>Email:</strong> {{ auth()->user()->email }}</p>
-    </x-filament::card>
+    <x-filament::grid>
+        <x-filament::card>
+            <dl class="text-sm text-gray-200">
+                <div class="mb-2">
+                    <dt class="font-medium text-white">Name</dt>
+                    <dd>{{ auth()->user()->name }}</dd>
+                </div>
+                <div>
+                    <dt class="font-medium text-white">Email</dt>
+                    <dd>{{ auth()->user()->email }}</dd>
+                </div>
+            </dl>
+        </x-filament::card>
+    </x-filament::grid>
 </x-filament::section>
