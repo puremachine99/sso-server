c5bc45c48024b51f8146eeefb61eaf41b250d4c4
Nvlqmn <puremachine99@gmail.com>
2025-08-06 12:13:10 +0700

    [PORTL-46] Bikin Display untuk user-profile

--- resources/views/components/display-field.blade.php
+++ resources/views/components/display-field.blade.php
@@ -1,6 +1,16 @@
-@props(['label' => '', 'value' => ''])
+@props([
+    'label' => '',
+    'value' => '',
+])
 
 <div class="space-y-1">
-    <label class="text-sm font-medium text-gray-700 dark:text-gray-300">{{ $label }}</label>
-    <div class="text-sm text-gray-900 dark:text-white">{{ $value }}</div>
+    @if ($label)
+        <label class="text-sm font-medium text-gray-500 dark:text-gray-400">
+            {{ $label }}
+        </label>
+    @endif
+
+    <div class="text-base text-gray-950 dark:text-white">
+        {{ is_string($value) || is_numeric($value) ? $value : '' }}
+    </div>
 </div>
