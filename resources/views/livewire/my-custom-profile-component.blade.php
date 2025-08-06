6fa6693e1fc6c57d2613077ed323793c8761fb44
Nvlqmn <puremachine99@gmail.com>
2025-08-06 11:14:31 +0700

    [PORTL-46] Bikin Display untuk user-profile

--- resources/views/livewire/my-custom-profile-component.blade.php
+++ resources/views/livewire/my-custom-profile-component.blade.php
@@ -1,24 +1,10 @@
-<x-filament::page>
-    <x-filament::section>
-        <x-filament::section.header title="Informasi Pengguna" />
+<x-filament::section>
+    <x-slot name="header">
+        <h2 class="text-lg font-bold">Profil Saya</h2>
+    </x-slot>
 
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
-            <div>
-                <x-filament::label value="Nama" />
-                <x-filament::input
-                    value="{{ auth()->user()?->name }}"
-                    disabled
-                    readonly
-                />
-            </div>
-            <div>
-                <x-filament::label value="Email" />
-                <x-filament::input
-                    value="{{ auth()->user()?->email }}"
-                    disabled
-                    readonly
-                />
-            </div>
-        </div>
-    </x-filament::section>
-</x-filament::page>
+    <x-filament::card>
+        <p><strong>Nama:</strong> {{ auth()->user()->name }}</p>
+        <p><strong>Email:</strong> {{ auth()->user()->email }}</p>
+    </x-filament::card>
+</x-filament::section>
