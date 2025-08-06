936c026e262a3f1bd8bd091303f8adca97730bcf
Nvlqmn <puremachine99@gmail.com>
2025-08-06 12:17:16 +0700

    [PORTL-46] Bikin Display untuk user-profile

--- resources/views/livewire/profile-component.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="fi-section fi-aside grid grid-cols-1 items-start gap-x-6 gap-y-4 md:grid-cols-3">
-    <x-filament::section>
-        {{ $this->form }}
-    </x-filament::section>
-</div>
